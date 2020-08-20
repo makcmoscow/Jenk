f57975b8656aa0b6606973e61442a74c6a61048f
makcmoscow <makcmoscow@gmail.com>
2020-08-20 13:09:04 +0300

    Update simple_script.py

--- simple_script.py
+++ simple_script.py
@@ -8,6 +8,6 @@ def test_check_digit():
   assert check_digit(123) == True, 'It\'s digit'
   assert check_digit('asd') == False, 'It\'s not a digit'
 
-if __name__ == __main__:
+if __name__ == '__main__':
   result = check_digit(345)
   print('result of function: ', result)
