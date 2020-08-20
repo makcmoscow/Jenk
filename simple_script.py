4e16cab47d4be183a2c2dc333cf71b1908562b42
makcmoscow <makcmoscow@gmail.com>
2020-08-20 12:58:25 +0300

    Update simple_script.py

--- simple_script.py
+++ simple_script.py
@@ -1,1 +1,13 @@
-print('hello')
+def check_digit(variable):
+  if variable.isdigit():
+    return True
+  else:
+    return False
+
+def test_check_digit():
+  assert check_digit(123) == True, 'It\'s digit'
+  assert check_digit('asd') == False, 'It\'s not a digit'
+
+if __name__ == __main__:
+  result = check_digit(345)
+  print('result of function: ', result)
