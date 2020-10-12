5bfb35b918c9ecaa98c6369ecb1d5ebfdddb3da2
makcmoscow <makcmoscow@gmail.com>
2020-10-12 23:11:50 +0300

    Update add_user_mailbox.py

--- add_user_mailbox.py
+++ add_user_mailbox.py
@@ -4,11 +4,12 @@ import argparse
 import random
 import time
 parser = argparse.ArgumentParser()
-#parser.add_argument('-d', type = int, default = 6, help = 'choose domain name')
+parser.add_argument('-d', '--domain', type = str, default = "m-production.tv", help = 'choose domain name')
 parser.add_argument('list_of_logins', type = str, help = 'Перечень имен в кавычках через запятую')
 args = parser.parse_args()
 list_of_logins = args.list_of_logins.split(',')
 log_in_cred = {'fUsername':'alexander@m-production.tv', 'fPassword' : '565ts89%w()32'}
+domain = args.domain
 
 
 def get_session(log_in_cred):
@@ -27,14 +28,14 @@ def create_mailbox(session, token, login, domain, password):
         password, 'value[password2]': password, 'value[active]': '1', 'value[welcome_mail]': '1', 'submit': 'Создать ящик'}
     rr = session.post('https://mail.m-pr.tv/postfixadmin/edit.php?table=mailbox&domain=abp-finances.ru', data = form_data)
 
-def get_domains(session):
-    domain_list = session.get('https://mail.m-pr.tv/postfixadmin/list.php?table=domain')
-    domain_list = str(domain_list.content.decode()).split('domain=')
-    domains = {}
-    for y, x in enumerate(domain_list):
-        if not y==0:
-            domains[y]=(x.split("\'")[0])
-    return domains
+#def get_domains(session):
+#    domain_list = session.get('https://mail.m-pr.tv/postfixadmin/list.php?table=domain')
+#    domain_list = str(domain_list.content.decode()).split('domain=')
+#    domains = {}
+#    for y, x in enumerate(domain_list):
+#        if not y==0:
+#            domains[y]=(x.split("\'")[0])
+#    return domains
 
 def create_password():
     chars = {1:'+-/*!&$#?=@<>', 2:'abcdefghijkopqrstuvwxyz', 3:'ABCDEFGHIJKLMNPQRSTUVWXYZ1234567890'}
@@ -46,11 +47,11 @@ def create_password():
 
 
 session = get_session(log_in_cred)
-domains = get_domains(session)
-for key in domains:
-    print(key, ' ', domains[key])
+#domains = get_domains(session)
+#for key in domains:
+#    print(key, ' ', domains[key])
 
-domain = domains[int(input('Enter domain number'))]
+#domain = domains[int(input('Enter domain number'))]
 
 for login in list_of_logins:
     login = login.strip()
